99cb14e83827a49d68a7efac9aca1ff4f148fb3c
aman-agrawal22 <114815980+aman-agrawal22@users.noreply.github.com>
2022-10-15 00:49:19 +0530

    Create Corrected_code.cpp

---
+++ Corrected_code.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+int32_t main()
+{
+    int a,b;
+    cin>>a>>b;
+    int result = a*b;
+    cout<<a*b<<endl;
+    return 0;
+}
